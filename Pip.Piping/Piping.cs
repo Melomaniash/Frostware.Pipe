3293725ae43eee2d67e4b6b8dde4f88aa6319844
Cameron <cameronproulx@outlook.com>
2021-02-14 16:54:58 -0500

    added summery tags

--- Pip.Piping/Piping.cs
+++ Pip.Piping/Piping.cs
@@ -4,19 +4,69 @@ namespace Pip.Piping
 {
     public static partial class Piping
     {
-        public static TResult Pipe<T, TResult>(this T input, Func<T, TResult> func)
-            => func(input);
+        /// <summary>
+        /// Performs the given function passing in the input as an argument
+        /// </summary>
+        /// <typeparam name="T">Input type</typeparam>
+        /// <typeparam name="TResult">Function return type</typeparam>
+        /// <param name="input">Input argument</param>
+        /// <param name="function">Function the pipe will perform on the inputs</param>
+        /// <returns>The function result</returns>
+        public static TResult Pipe<T, TResult>(this T input, Func<T, TResult> function)
+            => function(input);
 
-        public static TResult Pipe2<T1, T2, TResult>(this (T1 arg1, T2 arg2) input, Func<T1, T2, TResult> func)
-            => func(input.arg1, input.arg2);
+        /// <summary>
+        /// Performs the given function passing in 2 inputs as arguments
+        /// </summary>
+        /// <typeparam name="T1">First input type</typeparam>
+        /// <typeparam name="T2">Second input type</typeparam>
+        /// <typeparam name="TResult">Function return type</typeparam>
+        /// <param name="input">Input arguments</param>
+        /// <param name="function">Function the pipe will perform on the inputs</param>
+        /// <returns>The function result</returns>
+        public static TResult Pipe2<T1, T2, TResult>(this (T1 arg1, T2 arg2) input, Func<T1, T2, TResult> function)
+            => function(input.arg1, input.arg2);
 
-        public static TResult Pipe3<T1, T2, T3, TResult>(this (T1 arg1, T2 arg2, T3 arg3) input, Func<T1, T2, T3, TResult> func)
-           => func(input.arg1, input.arg2, input.arg3);
+        /// <summary>
+        /// Performs the given function passing in 3 inputs as arguments
+        /// </summary>
+        /// <typeparam name="T1">First input type</typeparam>
+        /// <typeparam name="T2">Second input type</typeparam>
+        /// <typeparam name="T3">Third input type</typeparam>
+        /// <typeparam name="TResult">Function return type</typeparam>
+        /// <param name="input">Input arguments</param>
+        /// <param name="function">Function the pipe will perform on the inputs</param>
+        /// <returns>The function result</returns>
+        public static TResult Pipe3<T1, T2, T3, TResult>(this (T1 arg1, T2 arg2, T3 arg3) input, Func<T1, T2, T3, TResult> function)
+           => function(input.arg1, input.arg2, input.arg3);
 
-        public static TResult Pipe4<T1, T2, T3, T4, TResult>(this (T1 arg1, T2 arg2, T3 arg3, T4 arg4) input, Func<T1, T2, T3, T4, TResult> func)
-          => func(input.arg1, input.arg2, input.arg3, input.arg4);
+        /// <summary>
+        /// Performs the given function passing in 4 inputs as arguments
+        /// </summary>
+        /// <typeparam name="T1">First input type</typeparam>
+        /// <typeparam name="T2">Second input type</typeparam>
+        /// <typeparam name="T3">Third input type</typeparam>
+        /// <typeparam name="T4">Fourth input type</typeparam>
+        /// <typeparam name="TResult">Function return type</typeparam>
+        /// <param name="input">Input arguments</param>
+        /// <param name="function">Function the pipe will perform on the inputs</param>
+        /// <returns>The function result</returns>
+        public static TResult Pipe4<T1, T2, T3, T4, TResult>(this (T1 arg1, T2 arg2, T3 arg3, T4 arg4) input, Func<T1, T2, T3, T4, TResult> function)
+          => function(input.arg1, input.arg2, input.arg3, input.arg4);
 
-        public static TResult Pipe5<T1, T2, T3, T4, T5, TResult>(this (T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5) input, Func<T1, T2, T3, T4, T5, TResult> func)
-          => func(input.arg1, input.arg2, input.arg3, input.arg4, input.arg5);
+        /// <summary>
+        /// Performs the given function passing in 4 inputs as arguments
+        /// </summary>
+        /// <typeparam name="T1">First input type</typeparam>
+        /// <typeparam name="T2">Second input type</typeparam>
+        /// <typeparam name="T3">Third input type</typeparam>
+        /// <typeparam name="T4">Fourth input type</typeparam>
+        /// <typeparam name="T5">Fifth input type</typeparam>
+        /// <typeparam name="TResult">Function return type</typeparam>
+        /// <param name="input">Input arguments</param>
+        /// <param name="function">Function the pipe will perform on the inputs</param>
+        /// <returns>The function result</returns>
+        public static TResult Pipe5<T1, T2, T3, T4, T5, TResult>(this (T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5) input, Func<T1, T2, T3, T4, T5, TResult> function)
+          => function(input.arg1, input.arg2, input.arg3, input.arg4, input.arg5);
     }
 }
--- Pip.Piping/PipingCurried.cs
+++ Pip.Piping/PipingCurried.cs
@@ -5,16 +5,58 @@ namespace Pip.Piping
 {
     public static partial class Piping
     {
-        public static Func<TArg2, TFuncResult> Pipe<TArg1, TArg2, TFuncResult>(this TArg1 input, Func<TArg1, TArg2, TFuncResult> func)
+        /// <summary>
+        /// Takes in a 2 argument function, curries it, then returns a partially applied function with input applied to the first argument
+        /// </summary>
+        /// <typeparam name="TArg1"></typeparam>
+        /// <typeparam name="TArg2"></typeparam>
+        /// <typeparam name="TFuncResult"></typeparam>
+        /// <param name="input"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static Func<TArg2, TFuncResult> PipeCurry<TArg1, TArg2, TFuncResult>(this TArg1 input, Func<TArg1, TArg2, TFuncResult> func)
             => func.Curry()(input);
 
-        public static Func<TArg2, Func<TArg3, TFuncResult>> Pipe<TArg1, TArg2, TArg3, TFuncResult>(this TArg1 input, Func<TArg1, TArg2, TArg3, TFuncResult> func)
+        /// <summary>
+        /// Takes in a 3 argument function, curries it, then returns a partially applied function with input applied to the first argument
+        /// </summary>
+        /// <typeparam name="TArg1"></typeparam>
+        /// <typeparam name="TArg2"></typeparam>
+        /// <typeparam name="TArg3"></typeparam>
+        /// <typeparam name="TFuncResult"></typeparam>
+        /// <param name="input"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static Func<TArg2, Func<TArg3, TFuncResult>> PipeCurry<TArg1, TArg2, TArg3, TFuncResult>(this TArg1 input, Func<TArg1, TArg2, TArg3, TFuncResult> func)
             => func.Curry()(input);
 
-        public static Func<TArg2, Func<TArg3, Func<TArg4, TFuncResult>>> Pipe<TArg1, TArg2, TArg3, TArg4, TFuncResult>(this TArg1 input, Func<TArg1, TArg2, TArg3, TArg4, TFuncResult> func)
+        /// <summary>
+        /// Takes in a 4 argument function, curries it, then returns a partially applied function with input applied to the first argument
+        /// </summary>
+        /// <typeparam name="TArg1"></typeparam>
+        /// <typeparam name="TArg2"></typeparam>
+        /// <typeparam name="TArg3"></typeparam>
+        /// <typeparam name="TArg4"></typeparam>
+        /// <typeparam name="TFuncResult"></typeparam>
+        /// <param name="input"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static Func<TArg2, Func<TArg3, Func<TArg4, TFuncResult>>> PipeCurry<TArg1, TArg2, TArg3, TArg4, TFuncResult>(this TArg1 input, Func<TArg1, TArg2, TArg3, TArg4, TFuncResult> func)
             => func.Curry()(input);
 
-        public static Func<TArg2, Func<TArg3, Func<TArg4, Func<TArg5, TFuncResult>>>> Pipe<TArg1, TArg2, TArg3, TArg4, TArg5, TFuncResult>(this TArg1 input, Func<TArg1, TArg2, TArg3, TArg4, TArg5, TFuncResult> func)
+        /// <summary>
+        /// Takes in a 5 argument function, curries it, then returns a partially applied function with input applied to the first argument
+        /// </summary>
+        /// <typeparam name="TArg1"></typeparam>
+        /// <typeparam name="TArg2"></typeparam>
+        /// <typeparam name="TArg3"></typeparam>
+        /// <typeparam name="TArg4"></typeparam>
+        /// <typeparam name="TArg5"></typeparam>
+        /// <typeparam name="TFuncResult"></typeparam>
+        /// <param name="input"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static Func<TArg2, Func<TArg3, Func<TArg4, Func<TArg5, TFuncResult>>>> PipeCurry<TArg1, TArg2, TArg3, TArg4, TArg5, TFuncResult>(this TArg1 input, Func<TArg1, TArg2, TArg3, TArg4, TArg5, TFuncResult> func)
             => func.Curry()(input);
     }
 }
